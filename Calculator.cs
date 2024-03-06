ce2684a455861208b0b24f88d1fd63bc236175f1
Raka40 <maulanaraka1030@gmail.com>
2024-03-06 17:00:08 +0700

    menambhakan fungsi penambahan

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,13 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-	
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b Penambahan(a, b));
+		static void penambahan (int a, int b)
+		{
+			return a + b;
+		}
 	}
 }
